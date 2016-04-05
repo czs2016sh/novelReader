c6058595f62f277d43f0aa4bfdca9caa37cf0164
cuizongshuai <1229390678@qq.com>
2016-04-05 10:59:51 +0800

    一行代码还没写呢

---
+++ novelReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  novelReader
+//
+//  Created by sq-ios53 on 16/4/5.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ novelReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  novelReader
+//
+//  Created by sq-ios53 on 16/4/5.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
